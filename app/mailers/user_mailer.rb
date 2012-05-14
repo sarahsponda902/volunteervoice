4e37509daf86109df9b41e5cb5e8a57dc007fb4e
Sarah Adams <sarahsponda902@gmail.com>
2012-05-14 16:13:27 -0700

    ah

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,28 +5,28 @@ class UserMailer < ActionMailer::Base
   
   def confirmation_instructions(resource)
       @options = {"promotion_name" => "ConfirmEmail", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "VolunteerVoice Confirmation Email"}
-      @yaml_body = {:instrux => "volunteervoice.herokuapp.com/confirmations/#{resource.confirmation_token}", :usrname => resource.username}
+      @yaml_body = {'instrux' => "volunteervoice.herokuapp.com/confirmations/#{resource.confirmation_token}", 'usrname' => resource.username}
       MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body.to_yaml)
       mail(:from => "no-reply@volunteervoice.org", :to => "new-user@volunteervoice.org")
   end
   
   def unlock_instructions(resource)
     @options = {"promotion_name" => "UnlockAccount", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "Unlock VolunteerVoice Account"}
-    @yaml_body = {:instrux => "volunteervoice.herokuapp.com/unlocks/#{resource.unlock_token}", :usrname => resource.username}
+    @yaml_body = {'instrux' => "volunteervoice.herokuapp.com/unlocks/#{resource.unlock_token}", 'usrname' => resource.username}
     MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body.to_yaml)
     mail(:from => "no-reply@volunteervoice.org", :to => "new-user@volunteervoice.org")
   end
   
   def reset_password_instructions(resource)
     @options = {"promotion_name" => "ResetPassword", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "Reset Password on VolunteerVoice Account"}
-    @yaml_body = {:instrux => "volunteervoice.herokuapp.com/passwords/#{resouce.edit_password_token}", :usrname => resource.username}
+    @yaml_body = {'instrux' => "volunteervoice.herokuapp.com/passwords/#{resouce.edit_password_token}", 'usrname' => resource.username}
     MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body.to_yaml)
     mail(:from => "no-reply@volunteervoice.org", :to => "new-user@volunteervoice.org")
   end
   
   def welcome_email(resource)
      @options = {"promotion_name" => "WelcomeEmail", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "Welcome to VolunteerVoice! You're Awesome."}
-     @yaml_body = {:hme => "volunteervoice.herokuapp.com", :usrname => resource.username}
+     @yaml_body = {'hme' => "volunteervoice.herokuapp.com", 'usrname' => resource.username}
      MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body.to_yaml)
      mail(:from => "no-reply@volunteervoice.org", :to => "new-user@volunteervoice.org")
   end
