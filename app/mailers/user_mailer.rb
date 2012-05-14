81aee4eb46eaaf76f5400de92059a478926e35c2
Sarah Adams <sarahsponda902@gmail.com>
2012-05-14 15:22:39 -0700

    S

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,31 +4,31 @@ class UserMailer < ActionMailer::Base
   require 'madmimi'
   
   def confirmation_instructions(resource)
-    :from => "no-reply@volunteervoice.org"
       @options = {"promotion_name" => "ConfirmEmail", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "VolunteerVoice Confirmation Email"}
       @yaml_body = {'instrux' => "/confirmations/#{resource.confirmation_token}", 'usrname' => resource.username}
       MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body)
+      mail(:from => "no-reply@volunteervoice.org")
   end
   
   def unlock_instructions(resource)
-    :from => "no-reply@volunteervoice.org"
     @options = {"promotion_name" => "UnlockAccount", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "Unlock VolunteerVoice Account"}
     @yaml_body = {'instrux' => "/unlocks/#{resource.unlock_token}", 'usrname' => resource.username}
     MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body)
+    mail(:from => "no-reply@volunteervoice.org")
   end
   
   def reset_password_instructions(resource)
-    :from => "no-reply@volunteervoice.org"
     @options = {"promotion_name" => "ResetPassword", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "Reset Password on VolunteerVoice Account"}
     @yaml_body = {'instrux' => "/passwords/#{resouce.edit_password_token}", 'usrname' => resource.username}
     MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body)
+    mail(:from => "no-reply@volunteervoice.org")
   end
   
   def welcome_email(resource)
-    :from => "no-reply@volunteervoice.org"
      @options = {"promotion_name" => "WelcomeEmail", "recipient" => resource.email, "from" => "no-reply@volunteervoice.org", "subject" => "Welcome to VolunteerVoice! You're Awesome."}
      @yaml_body = {'hme' => root_path, 'usrname' => resource.username}
      MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body)
+     mail(:from => "no-reply@volunteervoice.org")
   end
   
 end
