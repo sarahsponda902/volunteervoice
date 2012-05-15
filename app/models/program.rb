253ce64a7ad273d987539c5710cf2be9494b50de
Sarah Adams <sarahsponda902@gmail.com>
2012-05-15 01:08:38 -0700

    s

--- app/models/program.rb
+++ app/models/program.rb
@@ -38,7 +38,7 @@ def square_image_crop
     crop_params = "#{self.crop_w}x#{self.crop_h}+#{self.crop_x}+#{self.crop_y}"
     image.crop(crop_params)
     image.write "tempfile.jpg"
-    AWS::S3::S3Object.store(self.id.to_s+"_square.jpg", open("tempfile.jpg"), "volunteervoice_blogsquareimages", :access=>:public_read)
+    AWS::S3::S3Object.store(self.id.to_s+"_square.jpg", open("tempfile.jpg"), "volunteervoice_programsquareimages", :access=>:public_read)
     FileUtils.rm "tempfile.jpg"
     self.square_image = "https://s3.amazonaws.com/volunteervoice_blogsquareimages/#{self.id.to_s}_square.jpg"
   end
