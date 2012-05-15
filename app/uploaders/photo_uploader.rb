f6a01d00ea49c110e6b457807e6a1243fec48cbc
Sarah Adams <sarahsponda902@gmail.com>
2012-05-15 02:59:15 -0700

    sfds

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,7 +1,4 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   storage :fog
-  
-  def store_dir
-    'volunteervoice_uncropped'
-  end
+
 end
--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => ENV['AMAZON_ACCESS_KEY_ID'],       # copied off the aws site
     :aws_secret_access_key  => ENV['AMAZON_SECRET_ACCESS_KEY'],       # 
+    :region => 'us-east-1'
   }
-
-    config.fog_directory  = 'volunteervoice_uncropped'                     # required
+  config.fog_directory  = 'volunteervoice_uncropped'                     # required
 end
