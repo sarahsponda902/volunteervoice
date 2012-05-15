5f86f898aa26b37c409aca8df5804f3b3388c5d2
Sarah Adams <sarahsponda902@gmail.com>
2012-05-15 16:44:23 -0700

    s

--- app/models/blog_image.rb
+++ app/models/blog_image.rb
@@ -3,9 +3,6 @@ class BlogImage < ActiveRecord::Base
 
 	belongs_to :blog_post
 	
-	validates_file_extension_of :image, :allowed => ["jpg", "png", "jpeg", "gif"], :message => "must have one of the following extensions: jpg, jpeg, png, gif"
-	validates_file_size_of :image, :less_than => 1 * 1024 * 1024, :message => "must be no more than 1024x1024"
-
 	attr_accessor :random_id
 
 	# Check for paperclip
--- app/models/new_review.rb
+++ app/models/new_review.rb
@@ -4,10 +4,6 @@ class NewReview < ActiveRecord::Base
   characters."
   
   belongs_to :user
-  
-  validates_file_extension_of :photo, :allowed => ["jpg", "png", "jpeg", "gif"], :message => "must have one of the following extensions: jpg, jpeg, png, gif"
-	validates_file_size_of :photo, :less_than => 1 * 1024 * 1024, :message => "must be no more than 1024x1024"
-	before_create :image_save
 
   attr_accessible :body, :rating, :photo, :show, :time_frame, :before, :terms, :preparation, :support, :impact, :structure, :overall, :user_id, :organization, :program
 
@@ -17,11 +13,6 @@ mount_uploader :photo, ImageUploader
 
       def roundup(overall)
           (overall*2).round / 2.0
-      end
-  
-      def image_save
-        AWS::S3::S3Object.store(self.id.to_s+"_new_review.jpg", open(self.photo.path), "volunteervoice_uncropped", :access => :public_read)
-            self.photo = "https://s3.amazonaws.com/volunteervoice_uncropped/#{self.id.to_s}_new_review.jpg"
       end
   
 end
