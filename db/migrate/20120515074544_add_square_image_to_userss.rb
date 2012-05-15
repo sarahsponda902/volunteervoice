647ea269b0ffe55f784d402ec2ff21497f077742
Sarah Adams <sarahsponda902@gmail.com>
2012-05-15 01:52:14 -0700

    s

--- db/migrate/20120515074544_add_square_image_to_userss.rb
+++ db/migrate/20120515074544_add_square_image_to_userss.rb
@@ -1,14 +1,6 @@
 class AddSquareImageToUserss < ActiveRecord::Migration
   def change
     add_column :users, :square_image, :string
-    
-    remove_column :users, :square_photo_file_name
-    
-    remove_column :users, :square_photo_file_size
-    
-    remove_column :users, :square_photo_updated_at
-    
-    remove_column :users, :square_photo_content_size
 
   end
 end
