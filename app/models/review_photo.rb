d3ea19662838abd48526fbd0c0196b95eda2429d
Sarah Adams <sarahsponda902@gmail.com>
2012-05-16 15:09:18 -0700

    S

--- app/models/review_photo.rb
+++ app/models/review_photo.rb
@@ -1,6 +1,6 @@
 class ReviewPhoto < ActiveRecord::Base
   
-  attr_accessible :file, :review_id
+  attr_accessible :file, :review_id, :file_cache
   belongs_to :review
   mount_uploader :file, ReviewPhotoUploader
   
