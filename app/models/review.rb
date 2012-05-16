e8e49788b014f452df18901df2a33d44917da24d
Sarah Adams <sarahsponda902@gmail.com>
2012-05-16 16:39:03 -0700

    S

--- app/models/review.rb
+++ app/models/review.rb
@@ -12,7 +12,7 @@ belongs_to :user
 
 
 
-attr_accessible :user_id, :program_id, :body, :rating, :photo, :show, :organization_id, :time_frame, :before, :terms, :preparation, :support, :impact, :structure, :overall, :review_photos_attributes, :organization_name
+attr_accessible :user_id, :program_id, :body, :rating, :photo, :show, :organization_id, :time_frame, :before, :terms, :preparation, :support, :impact, :structure, :overall, :photo2, :photo3, :photo4, :photo5, :photo6, :photo7, :photo8, :photo9, :photo10, :organization_name
 
 # Paperclip
 mount_uploader :photo, ImageUploader
