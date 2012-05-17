ae217d4b6f4b60c1f1be834acddd94086b7b5d54
Sarah Adams <sarahsponda902@gmail.com>
2012-05-16 17:07:36 -0700

    S

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -26,16 +26,16 @@ class PagesController < ApplicationController
   def test
     @review = Review.find(12)
     @photos = []
-    @photos << @review.photo unless @review.photo.nil?
-    @photos << @review.photo2 unless @review.photo2.nil?
-    @photos << @review.photo3 unless @review.photo3.nil?
-    @photos << @review.photo4 unless @review.photo4.nil?
-    @photos << @review.photo5 unless @review.photo5.nil?
-    @photos << @review.photo6 unless @review.photo6.nil?
-    @photos << @review.photo7 unless @review.photo7.nil?
-    @photos << @review.photo8 unless @review.photo8.nil?
-    @photos << @review.photo9 unless @review.photo9.nil?
-    @photos << @review.photo10 unless @review.photo10.nil?
+    @photos << @review.photo unless @review.photo.blank?
+    @photos << @review.photo2 unless @review.photo2.blank?
+    @photos << @review.photo3 unless @review.photo3.blank?
+    @photos << @review.photo4 unless @review.photo4.blank?
+    @photos << @review.photo5 unless @review.photo5.blank?
+    @photos << @review.photo6 unless @review.photo6.blank?
+    @photos << @review.photo7 unless @review.photo7.blank?
+    @photos << @review.photo8 unless @review.photo8.blank?
+    @photos << @review.photo9 unless @review.photo9.blank?
+    @photos << @review.photo10 unless @review.photo10.blank?
     
   end
   	
