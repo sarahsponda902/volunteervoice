767c73235081ad7c35406fb05091f62d4cef1b61
Sarah Adams <sarahsponda902@gmail.com>
2012-05-22 17:34:21 -0700

    S

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,15 @@
 class Feedback < ActiveRecord::Base
   belongs_to :page
   attr_accessible :body, :user_id, :show, :email
+  validates_length_of :body, :minimum => 5
+  before_save validates_email
+  
+  def validates_email
+    if !self.user_id
+      if !self.email
+        return false
+      end
+    end
+  end
+  
 end
