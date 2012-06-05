ec03aa9eb2a64d7bebd04bc513d6712b47f957b5
Sarah Adams <sarahsponda902@gmail.com>
2012-06-05 12:35:36 -0700

    H

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,7 +5,7 @@ class Organization < ActiveRecord::Base
 has_many :comments
 has_many :programs
 has_many :review_os
-attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model, :good_to_know, :reviews_count, :trining_resources, :program_costs, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image
+attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model_string, :good_to_know, :reviews_count, :trining_resources, :price_ranges, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image
 validates_presence_of :image
 
 scope :random, :order=>'RAND()', :limit=>1
@@ -21,6 +21,8 @@ before_create :set_page_views_to_zero
 # Paperclip
     mount_uploader :image, ImageUploader
     mount_uploader :square_image, ImageUploader
+    mount_uploader :price_breakdown, ImageUploader
+    mount_uploader :price_ranges, ImageUploader
 # Sunspot Search
 searchable do
   text :name, :boost =>9
