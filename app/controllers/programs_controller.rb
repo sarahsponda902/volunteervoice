7b3246850bcef2f2ea944766e99ad54f3a121bc7
Sarah Adams <sarahsponda902@gmail.com>
2012-06-07 15:00:53 -0700

    G

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1055,9 +1055,6 @@ class ProgramsController < ApplicationController
     @program.cost_includes = RedCloth.new( ActionController::Base.helpers.sanitize( @program.cost_includes ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
     @program.cost_doesnt_include = RedCloth.new( ActionController::Base.helpers.sanitize( @program.cost_doesnt_include ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
     
-    @program.length = @the_length
-    @program.group_size = @the_size
-    
     if (@program.overall.nil?) 
       @program.overall= 0;
     end
