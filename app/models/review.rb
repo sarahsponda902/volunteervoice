4688e95daa52154192e350264d80bd3d53b42bee
Sarah Adams <sarahsponda902@gmail.com>
2012-06-08 00:27:37 -0700

    H

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,16 @@
 class Review < ActiveRecord::Base
-  
+
+@orgs = []
+Organization.all.each do |f|
+  @orgs << f.name
+end
   
 validates :terms, :acceptance => {:accept => true}
-validates :organization_id, :presence => true
 validates :program_id, :presence => true
 validates_length_of :body, :minimum => 200, :message => "Must contain at least 30 
 characters."
 validates_length_of :body, :maximum => 10000, :message => "You have entered more than 10,000 characters"
+validates_inclusion_of :organization_name, :in => @orgs
 belongs_to :program
 belongs_to :user
 
