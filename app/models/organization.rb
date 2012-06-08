8d951eea03abfe18e20439c3a75522b06a61f17c
Sarah Adams <sarahsponda902@gmail.com>
2012-06-08 12:38:12 -0700

    G

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,7 +4,7 @@ class Organization < ActiveRecord::Base
   
 has_many :comments
 has_many :programs, :dependent => :destroy
-attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model_string, :good_to_know, :reviews_count, :trining_resources, :price_ranges, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image, :program_costs_doesnt_include, :price_breakdown
+attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model_string, :good_to_know, :reviews_count, :trining_resources, :price_ranges, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image, :program_costs_doesnt_include, :price_breakdown, :headquarters_location
 validates_presence_of :image
 
 scope :random, :order=>'RAND()', :limit=>1
