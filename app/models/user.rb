7c19e47ff32c4d4f3410dad582f243d664356dfd
Sarah Adams <sarahsponda902@gmail.com>
2012-06-08 02:06:11 -0700

    G

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
   has_many :favorites
   validates_uniqueness_of :username
   validates_uniqueness_of :email, :message => "is already in use"
-  validates_presence_of :username, :email, :dob
+  validates_presence_of :username, :dob
   validates_length_of :username, :maximum => 30
   validates_format_of :email, :with => %r{.+@.+\..+}, :message => "is not valid"
   
