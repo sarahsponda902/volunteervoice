b2abf1fa5fec1756ebc6dbb7ea0d4d32750f7ab2
Sarah Adams <sarahsponda902@gmail.com>
2012-06-08 11:38:42 -0700

    G

--- db/migrate/20120608174905_remove_headquarters_location_from_organization.rb
+++
@@ -1,9 +0,0 @@
-class RemoveHeadquartersLocationFromOrganization < ActiveRecord::Migration
-  def up
-    remove_column :organizations, :headquarters_location
-      end
-
-  def down
-    add_column :organizations, :headquarters_location, :string
-  end
-end
