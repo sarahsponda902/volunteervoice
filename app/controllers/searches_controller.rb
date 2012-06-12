0a8ed45d7003d990a16d64e499f779a73605435d
Sarah Adams <sarahsponda902@gmail.com>
2012-06-12 12:44:59 -0700

    G

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -575,43 +575,43 @@ class SearchesController < ApplicationController
         end
 
         if params['subject'].include?('Agriculture')
-          params['subject'] = ['Organic Farming', 'Sustainable Development'] + params['subject'].to_a
+          params['subject'] = ['Organic Farming', 'Sustainable Development'].concat(params['subject'])
         end
         if params['subject'].include?('Animal Care')
-          params['subject'] = ['Animal Rights', 'Wildlife Conservation'] + params['subject'].to_a
+          params['subject'] = ['Animal Rights', 'Wildlife Conservation'].concat(params['subject'])
         end
         if params['subject'].include?('Caregiving')
-          params['subject'] = ['Elder Care', 'Child/Orphan Care', 'Disabled Care', 'Feed the Homeless'] + params['subject'].to_a
+          params['subject'] = ['Elder Care', 'Child/Orphan Care', 'Disabled Care', 'Feed the Homeless'].concat(params['subject'])
         end
         if params['subject'].include?('Community Development')
-          params['subject'] = params['subject']+['Youth Development and Outreach'] + params['subject'].to_a
+          params['subject'] = params['subject']+['Youth Development and Outreach'].concat(params['subject'])
         end
         if params['subject'].include?('Culture and Community')
-          params['subject'] = ['Performing Arts', 'Fashion', 'Music', 'Sports & Recreation', 'Journalism'] + params['subject'].to_a
+          params['subject'] = ['Performing Arts', 'Fashion', 'Music', 'Sports & Recreation', 'Journalism'].concat(params['subject'])
         end
         if params['subject'].include?('Disaster Relief')
-          params['subject'] = ['Economics', 'Microfinance'] + params['subject'].to_a
+          params['subject'] = ['Economics', 'Microfinance'].concat(params['subject'])
         end
         if params['subject'].include?('Education')
-          params['subject'] = ['Teaching Buddhist Monks', 'Teaching Children', 'Teaching Computer Literacy'] + params['subject'].to_a
+          params['subject'] = ['Teaching Buddhist Monks', 'Teaching Children', 'Teaching Computer Literacy'].concat(params['subject'])
         end
         if params['subject'].include?('Environmental')
-          params['subject'] = ['Ecological Conservation', 'Sustainable Development', 'Wildlife Conservation', 'Habitat Restoration'] + params['subject'].to_a
+          params['subject'] = ['Ecological Conservation', 'Sustainable Development', 'Wildlife Conservation', 'Habitat Restoration'].concat(params['subject'])
         end
         if params['subject'].include?('Health and Medicine')
-          params['subject'] = ['HIV/AIDS', 'Family Planning', 'Nutrition', 'Veterinary Medicine', 'Clinical Work', 'Dental Work', 'Medical Research', 'Health Education', 'Public Health', 'Hospital Caregiving'] + params['subject'].to_a
+          params['subject'] = ['HIV/AIDS', 'Family Planning', 'Nutrition', 'Veterinary Medicine', 'Clinical Work', 'Dental Work', 'Medical Research', 'Health Education', 'Public Health', 'Hospital Caregiving'].concat(params['subject'])
         end
         if params['subject'].include?('Human Rights')
-          params['subject'] = ['Womens Initiatives'] + params['subject'].to_a
+          params['subject'] = ['Womens Initiatives'].concat(params['subject'])
         end
         if params['subject'].include?('Recreation')
-          params['subject'] = ['Adventure Travel'] + params['subject'].to_a
+          params['subject'] = ['Adventure Travel'].concat(params['subject'])
         end
         if params['subject'].include?('Scientific Research')
-          params['subject'] = ['Archaeology', 'Environmental Biology'] + params['subject'].to_a
+          params['subject'] = ['Archaeology', 'Environmental Biology'].concat(params['subject'])
         end
         if params['subject'].include?('Technology')
-          params['subject'] = ['Teaching Computer Literacy', 'Media Marketing and Graphic Design'] + params['subject'].to_a
+          params['subject'] = ['Teaching Computer Literacy', 'Media Marketing and Graphic Design'].concat(params['subject'])
         end
     end
     
@@ -1475,8 +1475,9 @@ class SearchesController < ApplicationController
 
         else
           if params[:subject].class.name == "String"
-            params[:subject] = params[:subject].to_a
+            params[:subject] = [].concat(params[:subject])
           end
+          
             if params[:region].nil?
               @search = Program.search do
 
