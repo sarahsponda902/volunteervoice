d5b6549c5c47175632b2db97796b9f44efdc1e8e
Sarah Adams <sarahsponda902@gmail.com>
2012-06-12 14:43:54 -0700

    G

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   apply_simple_captcha :message => "did not match the secret code", :distortion => "high"
   
   attr_accessible :email, :password, :password_confirmation, :remember_me, :photo, :username, :id, :age, :country, :dob, :notify, :square_photo, :crop_x, :crop_y, :crop_w, :crop_h, :captcha, :captcha_key, :approved, :volunteered_before, :admin_update, :admin_pass, :messages_show, :profile_show, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :confirmation_token, :crops, :square_image 
-  has_many :messages, :dependent => :destroy
+  has_many :messages
   has_many :reviews, :dependent => :destroy
   has_many :favorites, :dependent => :destroy
   validates_uniqueness_of :username
