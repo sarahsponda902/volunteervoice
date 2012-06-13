f4c0c2348b347a368d1b780955aaeae4f181d915
Sarah Adams <sarahsponda902@gmail.com>
2012-06-13 02:34:41 -0700

    G

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -96,7 +96,7 @@ class ReviewsController < ApplicationController
         @review.truncated100 = RedCloth.new( ActionController::Base.helpers.sanitize( (@review.body[0..99] + "...") ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
         @review.truncated200 = RedCloth.new( ActionController::Base.helpers.sanitize( (@review.body[0..199] + "...") ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
 
-         @review.body = RedCloth.new( sanitize( @review.body ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html.html_safe
+        params[:review][:body] = @review.body + RedCloth.new( sanitize( params[:review][:body] ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html.html_safe
        
        if user_signed_in? && (current_user.admin? || current_user.id == @review.user_id)
 
