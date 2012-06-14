f337a49309b3a3c30b6b851fffc8628a2c7ea058
Sarah Adams <sarahsponda902@gmail.com>
2012-06-13 18:45:37 -0700

    G

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -17,47 +17,6 @@ $(function(){
 	});
 
 
-
-	$('button.add').click(function(){
-		slider
-			.append( panel.replace(/\*2/g, ++imageNumber).replace(/\*1/g, (imageNumber%7 + 1)) )
-			.movingBoxes(); // update movingBoxes
-		navLinks(); // update go to panel links
-	});
-
-	// Remove a slide
-	$('button.remove').click(function(){
-		var d = slider.data('movingBoxes'),
-			c = d.curPanel,
-			t = d.totalPanels;
-		if (t > 1) {
-			slider.find('.mb-panel:not(.cloned):last').remove();
-			if (c > t - 1) { c = t - 1; }
-			slider.movingBoxes(); // update movingBoxes
-		}
-		navLinks();
-	});
-
-	navLinks();
-
-	// Examples of how to move the panel from outside the plugin.
-	// get (all 3 examples do exactly the same thing):
-	//  var currentPanel = $('#slider-one').data('movingBoxes').currentPanel(); // returns # of currently selected/enlarged panel
-	//  var currentPanel = $('#slider-one').data('movingBoxes').curPanel; // get the current panel number directly
-	//  var currentPanel = $('#slider-one').getMovingBoxes().curPanel; // use internal function to return the object (essentially the same as the line above)
-
-	// set (all 4 examples do exactly the same thing):
-	//  var currentPanel = $('#slider-one').data('movingBoxes').currentPanel(2, function(){ alert('done!'); }); // returns and scrolls to 2nd panel
-	//  var currentPanel = $('#slider-one').getMovingBoxes().currentPanel(2, function(){ alert('done!'); }); // just like the line above
-	//  var currentPanel = $('#slider-one').movingBoxes(2, function(){ alert('done!'); }); // scrolls to 2nd panel, runs callback & returns 2.
-	//  var currentPanel = $('#slider-one').getMovingBoxes().change(2, function(){ alert('done!'); }); // internal change function is the main function
-
-	$('.dlinks').delegate('a', 'click', function(){
-		// slider # stored in the text
-		slider.movingBoxes( $(this).text() );
-		return false;
-	});
-
 	// Report events to firebug console
 	$('.mb-slider').bind('initialized.movingBoxes initChange.movingBoxes beforeAnimation.movingBoxes completed.movingBoxes',function(e, slider, tar){
 		// show object ID + event in the firebug console
