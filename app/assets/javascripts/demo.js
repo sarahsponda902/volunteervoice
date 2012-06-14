3a246168268d4f457f421bd21b4b38055ada3d15
Sarah Adams <sarahsponda902@gmail.com>
2012-06-13 17:44:01 -0700

    G

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -4,19 +4,6 @@
 
 $(function(){
 
-	$('#slider-one').movingBoxes({
-		startPanel   : 1,      // start with this panel
-		reducedSize  : 0.8,    // non-current panel size: 80% of panel size
-		wrap         : true,   // if true, the panel will "wrap" (it really rewinds/fast forwards) at the ends
-		buildNav     : true,   // if true, navigation links will be added
-		navFormatter : function(){ return "&#9679;"; } // function which returns the navigation text for each panel
-
-		// width and panelWidth options removed in v2.2.2, but still backwards compatible
-		// width        : 300,    // overall width of movingBoxes (not including navigation arrows)
-		// panelWidth   : 0.5,    // current panel width
-
-	});
-
 	$('#slider-two').movingBoxes({
 		startPanel   : 1,      // start with this panel
 		reducedSize  : 0.8,    // non-current panel size: 80% of panel size
