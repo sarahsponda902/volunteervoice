3502b077a169b1f2c2f226b3ceab297da0147b7e
Sarah Adams <sarahsponda902@gmail.com>
2012-06-19 23:04:13 -0700

    G

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,6 @@ class ApplicationController < ActionController::Base
 include SimpleCaptcha::ControllerHelpers
 
    def after_sign_in_path_for(resource_or_scope)
-     if request.env['omniauth.origin']
-       request.env['omniauth.origin']
-     else
-       '/'
-     end
+     :back
    end
 end
