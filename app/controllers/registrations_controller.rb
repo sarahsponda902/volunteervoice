108c1a4ba9b1306f7cecdc15b19fee842c44ed85
Sarah Adams <sarahsponda902@gmail.com>
2012-06-20 12:42:59 -0700

    G

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -43,23 +43,15 @@ class RegistrationsController < Devise::RegistrationsController
   end
   
   def must_be
-    build_resource
-    resource.unread_messages = 0
-     if resource.save_with_captcha
-           if resource.confirmed?
-             set_flash_message :notice, :signed_up if is_navigational_format?
-             sign_in(resource_name, resource)
-             respond_with resource, :location => after_sign_up_path_for(resource)
-           else
-             set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-             expire_session_data_after_sign_in!
-             respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-           end
-    else
-      flash[:notice] = flash[:notice].to_a.concat resource.errors.full_messages
-      clean_up_passwords resource
-      respond_with resource
-    end
+    @link = URI(request.referrer).path
+    resource = build_resource({})
+    respond_with resource
+  end
+  
+  def new
+    @link = URI(request.referrer).path
+    resource = build_resource({})
+    respond_with resource
   end
 
 
