cfcba79243e0749894b292018ae8cefe19b3b434
Sarah Adams <sarahsponda902@gmail.com>
2012-06-19 23:01:42 -0700

    G

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 include SimpleCaptcha::ControllerHelpers
 
    def after_sign_in_path_for(resource_or_scope)
-     "/"
+     if request.env['omniauth.origin']
+       request.env['omniauth.origin']
+     else
+       '/'
+     end
    end
 end
--- app/controllers/sessions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SessionsController < Devise::SessionsController 
-  
-  def after_sign_in_path_for(resource)
-    :back
-  end
-  
-end
