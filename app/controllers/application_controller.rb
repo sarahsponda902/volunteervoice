f33f9a8a47cc26af623afaf1a63a6e1c206597c2
Sarah Adams <sarahsponda902@gmail.com>
2012-06-19 23:08:19 -0700

    G

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 include SimpleCaptcha::ControllerHelpers
 
    def after_sign_in_path_for(resource_or_scope)
-     :back
+     request.referrer
    end
 end
