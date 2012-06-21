ca4a4d124e24381501ad6f6af7fb97b2e63a7de4
Sarah Adams <sarahsponda902@gmail.com>
2012-06-21 14:38:00 -0700

    G

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
     @show_reviews = @show_reviews_all[0..2]
     end
     if !(@organizations.nil?)
-    @show_organizations_all = @organizations.sort_by(&:page_views)
+    @show_organizations_all = @organizations.sort_by(&:overall)
     @show_organizations = @show_organizations_all[0..1]
     end
     if !(@feedbacks.nil?)
