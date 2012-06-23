bbdde1dbb2fe9fd1f4295b23d56c52bf9364648e
Sarah Adams <sarahsponda902@gmail.com>
2012-06-23 12:27:00 -0700

    G

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -797,52 +797,52 @@ class SearchesController < ApplicationController
   # PUT /searches/1
   # PUT /searches/1.json
   def update
-    @search = Search.find(params[:id])
-
+    @the_search = Search.find(params[:id])
     
     
-    if @search.subjects.include?('Agriculture')
-      @search.subjects = ['Organic Farming', 'Sustainable Development'] + (@search.subjects.split("; "))
+    
+    if params[:search][:subjects].include?('Agriculture')
+      params[:search][:subjects] = ['Organic Farming', 'Sustainable Development'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Animal Care')
-      @search.subjects = ['Animal Rights', 'Wildlife Conservation'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Animal Care')
+      params[:search][:subjects] = ['Animal Rights', 'Wildlife Conservation'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Caregiving')
-      @search.subjects = ['Elder Care', 'Child/Orphan Care', 'Disabled Care', 'Feed the Homeless'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Caregiving')
+      params[:search][:subjects] = ['Elder Care', 'Child/Orphan Care', 'Disabled Care', 'Feed the Homeless'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Community Development')
-      @search.subjects = ['Youth Development and Outreach'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Community Development')
+      params[:search][:subjects] = ['Youth Development and Outreach'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Culture and Community')
-      @search.subjects = ['Performing Arts', 'Fashion', 'Music', 'Sports & Recreation', 'Journalism'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Culture and Community')
+      params[:search][:subjects] = ['Performing Arts', 'Fashion', 'Music', 'Sports & Recreation', 'Journalism'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Disaster Relief')
-      @search.subjects = ['Economics', 'Microfinance'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Disaster Relief')
+      params[:search][:subjects] = ['Economics', 'Microfinance'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Education')
-      @search.subjects = ['Teaching Buddhist Monks', 'Teaching Children', 'Teaching Computer Literacy'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Education')
+      params[:search][:subjects] = ['Teaching Buddhist Monks', 'Teaching Children', 'Teaching Computer Literacy'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Environmental')
-      @search.subjects = ['Ecological Conservation', 'Sustainable Development', 'Wildlife Conservation', 'Habitat Restoration'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Environmental')
+      params[:search][:subjects] = ['Ecological Conservation', 'Sustainable Development', 'Wildlife Conservation', 'Habitat Restoration'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Health and Medicine')
-      @search.subjects = ['HIV/AIDS', 'Family Planning', 'Nutrition', 'Veterinary Medicine', 'Clinical Work', 'Dental Work', 'Medical Research', 'Health Education', 'Public Health', 'Hospital Caregiving'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Health and Medicine')
+      params[:search][:subjects] = ['HIV/AIDS', 'Family Planning', 'Nutrition', 'Veterinary Medicine', 'Clinical Work', 'Dental Work', 'Medical Research', 'Health Education', 'Public Health', 'Hospital Caregiving'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Human Rights')
-      @search.subjects = ['Womens Initiatives'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Human Rights')
+      params[:search][:subjects] = ['Womens Initiatives'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Recreation')
-      @search.subjects = ['Adventure Travel'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Recreation')
+      params[:search][:subjects] = ['Adventure Travel'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Scientific Research')
-      @search.subjects = ['Archaeology', 'Environmental Biology'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Scientific Research')
+      params[:search][:subjects] = ['Archaeology', 'Environmental Biology'] + (params[:search][:subjects].split("; "))
     end
-    if @search.subjects.include?('Technology')
-      @search.subjects = ['Teaching Computer Literacy', 'Media Marketing and Graphic Design'] + (@search.subjects.split("; "))
+    if params[:search][:subjects].include?('Technology')
+      params[:search][:subjects] = ['Teaching Computer Literacy', 'Media Marketing and Graphic Design'] + (params[:search][:subjects].split("; "))
     end
     
-    if @search.subjects.include?("false")
-      @search.subjects = ['Agriculture', 
+    if params[:search][:subjects].include?("false")
+      params[:search][:subjects] = ['Agriculture', 
         'Organic Farming', 
         'Sustainable Development', 
         'Animal Care', 
@@ -900,8 +900,8 @@ class SearchesController < ApplicationController
         'Media Marketing and Graphic Design']
     end
     
-    if (@search.regions.include?('Americas'))
-      @search.regions = ['DZ', 
+    if (params[:search][:regions].include?('Americas'))
+      params[:search][:regions] = ['DZ', 
       'EG', 
       'LY', 
       'MA', 
@@ -957,11 +957,11 @@ class SearchesController < ApplicationController
       'LS', 
       'NA', 
       'ZA', 
-      'SZ'] + @search.regions.split("; ")
+      'SZ'] + params[:search][:regions].split("; ")
     end
 
-    if (@search.regions.include?('Europe'))
-      @search.regions = ['AX', 
+    if (params[:search][:regions].include?('Europe'))
+      params[:search][:regions] = ['AX', 
       'DK', 
       'EE', 
       'FO', 
@@ -1011,11 +1011,11 @@ class SearchesController < ApplicationController
       'SM', 
       'RS', 
       'SI', 
-      'ES'] + @search.regions.split("; ")
+      'ES'] + params[:search][:regions].split("; ")
     end
 
-    if (@search.regions.include?('Africa'))
-      @search.regions = ['BM', 
+    if (params[:search][:regions].include?('Africa'))
+      params[:search][:regions] = ['BM', 
       'CA', 
       'GL', 
       'PM', 
@@ -1066,11 +1066,11 @@ class SearchesController < ApplicationController
       'PE', 
       'SR', 
       'UY', 
-      'VE'] + @search.regions.split("; ")
+      'VE'] + params[:search][:regions].split("; ")
     end
 
-    if (@search.regions.include?('Asia'))
-      @search.regions = ['KZ', 
+    if (params[:search][:regions].include?('Asia'))
+      params[:search][:regions] = ['KZ', 
       'KG', 
       'TJ', 
       'TM', 
@@ -1120,11 +1120,11 @@ class SearchesController < ApplicationController
       'SY', 
       'TR', 
       'AE', 
-      'YE'] + @search.regions.split("; ")
+      'YE'] + params[:search][:regions].split("; ")
     end
 
-    if (@search.regions.include?('Oceania'))
-      @search.regions = ['AU', 
+    if (params[:search][:regions].include?('Oceania'))
+      params[:search][:regions] = ['AU', 
       'NZ', 
       'NF', 
       'FJ', 
@@ -1148,12 +1148,12 @@ class SearchesController < ApplicationController
       'TK', 
       'TO', 
       'TV', 
-      'WF'] + @search.regions.split("; ")
+      'WF'] + params[:search][:regions].split("; ")
     end
     
     
-    if @search.regions.include?("false")
-      @search.regions = ['DZ', 
+    if params[:search][:regions].include?("false")
+      params[:search][:regions] = ['DZ', 
       'EG', 
       'LY', 
       'MA', 
@@ -1390,11 +1390,11 @@ class SearchesController < ApplicationController
       'TV', 
       'WF']
     end
-    if @search.lengths.include?("false")
-       @search.lengths = ["1 week or less", "2-4 weeks", "5-8 weeks", "9-12 weeks", "3-6 months", "6-12 months", "1-2 years", "2+ years"]
+    if params[:search][:lengths].include?("false")
+       params[:search][:lengths] = ["1 week or less", "2-4 weeks", "5-8 weeks", "9-12 weeks", "3-6 months", "6-12 months", "1-2 years", "2+ years"]
     end
-    if @search.sizes.include?("false")
-      @search.sizes = ["Individual", "Small Groups (2-3)", "Medium Groups (4-10)", "Large Groups (11+)"]
+    if params[:search][:sizes].include?("false")
+      params[:search][:sizes] = ["Individual", "Small Groups (2-3)", "Medium Groups (4-10)", "Large Groups (11+)"]
     end
     
     
