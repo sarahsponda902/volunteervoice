5c6a7df06e4e7e44f5a9374fcc20fbcbf191c8a4
Sarah Adams <sarahsponda902@gmail.com>
2012-06-24 15:43:40 -0700

    G

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 RMTest::Application.routes.draw do
   
+  unless Rails.application.config.consider_all_requests_local
+      match '*not_found', to: 'errors#error_404'
+  end
+  
   get "errors/error_404"
 
   get "errors/error_500"
