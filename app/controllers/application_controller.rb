722117a7c7159e7e3e6d9a336bb19c6521365f5b
Sarah Adams <sarahsponda902@gmail.com>
2012-06-24 21:39:43 -0700

    G

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,14 +37,12 @@ include SimpleCaptcha::ControllerHelpers
    def after_update_path_for(resource)
      "/pages/profile"
    end
-   
-   unless Rails.application.config.consider_all_requests_local
+
        rescue_from Exception, with: :render_500
        rescue_from ActionController::RoutingError, with: :render_404
        rescue_from ActionController::UnknownController, with: :render_404
        rescue_from ActionController::UnknownAction, with: :render_404
-       rescue_from ActiveRecord::RecordNotFound, with: :render_404       
-   end
+       rescue_from ActiveRecord::RecordNotFound, with: :render_404     
 
      private
      def render_404(exception)
