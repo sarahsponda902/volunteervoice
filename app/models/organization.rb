27f1e44a2ebfe4ffeac5a2eb3d68bad6d2fc4a43
Sarah Adams <sarahsponda902@gmail.com>
2012-06-29 12:43:03 -0700

    G

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -6,7 +6,7 @@ has_many :program_cost_length_maps, :through => :programs
 has_many :program_sizes, :through => :programs
 has_many :program_subjects, :through => :programs
 has_many :programs, :dependent => :destroy
-attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model_string, :good_to_know, :reviews_count, :trining_resources, :price_ranges, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image, :program_costs_doesnt_include, :price_breakdown, :headquarters_location, :program_subjects
+attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model_string, :good_to_know, :reviews_count, :trining_resources, :price_ranges, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image, :program_costs_doesnt_include, :price_breakdown, :headquarters_location, :types_of_programs
 validates_presence_of :image
 
 scope :random, :order=>'RAND()', :limit=>1
