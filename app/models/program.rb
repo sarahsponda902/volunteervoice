5dd5cc359d923c799b82bf0872e171beba05fc98
Sarah Adams <sarahsponda902@gmail.com>
2012-06-29 18:30:32 -0700

    G

--- app/models/program.rb
+++ app/models/program.rb
@@ -28,7 +28,7 @@ searchable do
     program_sizes.map(&:size)
   end
   string :location
-  latlon :program_cost_length_maps, :multiple => true do
+  location :program_cost_length_maps, :multiple => true do
     program_cost_length_maps.map{|p| Sunspot::Util::Coordinates.new(p.length.to_f, p.cost.to_f) }
   end
   
