aaf8dc2f18354908d7b073ad95e5b7565301bc32
Sarah Adams <sarahsponda902@gmail.com>
2012-06-29 18:21:52 -0700

    G

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -72,14 +72,14 @@ class SearchesController < ApplicationController
       price_min = @search.price_min
     end
     if (@search.length_min_number.nil? || @search.length_min_param.nil?)
-      length_min = 0.weeks.to_i
+      length_min = 0.weeks.to_f
     else
-      length_min = @search.length_min_number.to_i.send(@search.length_min_param).to_i
+      length_min = @search.length_min_number.to_i.send(@search.length_min_param).to_f
     end
     if (@search.length_max_number.nil? || @search.length_max_param.nil?)
-      length_max = 2.years.to_i
+      length_max = 2.years.to_f
     else
-      length_max = @search.length_max_number.to_i.send(@search.length_max_param).to_i
+      length_max = @search.length_max_number.to_i.send(@search.length_max_param).to_f
     end
     
       
