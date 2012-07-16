dcbc853246dbd97ac815264edb54e07f72b26c24
Sarah Adams <sarahsponda902@gmail.com>
2012-07-16 11:29:40 -0700

    G

--- app/controllers/mad_mimi_emails_controller.rb
+++ app/controllers/mad_mimi_emails_controller.rb
@@ -42,10 +42,21 @@ class MadMimiEmailsController < ApplicationController
   # POST /mad_mimi_emails.json
   def create
     @mad_mimi_email = MadMimiEmail.new(params[:mad_mimi_email])
-    @options = {"promotion_name" => @mad_mimi_email.promotion_name, "recipients" => @mad_mimi_email.list_names, "from" => @mad_mimi_email.from, "subject" => @mad_mimi_email.subject}
-    @yaml_body = {}
-    @saved = MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body)
-
+    if @mad_mimi_email.list_names == "users"
+      @mad_mimi_email.list_names = User.where(:notify => true).map{|user| [user.email, {'username' => user.username, 'email' => user.email}]}
+    end
+    if @mad_mimi_email.list_names == "admins"
+      @mad_mimi_email.list_names = User.where(:admin => true).map{|admin| [admin.email, {'username' => admin.username, 'email' => admin.email}]}
+    end
+    if @mad_mimi_email.list_names == "organizations"
+      @mad_mimi_email.list_names = OrganizationAccount.where(:notify => true).map{|org| [org.email, {'firstname' => org.first_name, 'lastname' => org.last_name, 'position' => org.position, 'email' => org.email, 'username' => org.username, 'country' => org.country, 'organization_name' => Organization.find(org.organization_id).name}]}
+    end
+    
+    @mad_mimi_email.list_names.each do |f|
+      @options = {"promotion_name" => @mad_mimi_email.promotion_name, "recipients" => f[0], "from" => @mad_mimi_email.from, "subject" => @mad_mimi_email.subject}
+      @yaml_body = f[1]
+      @saved = MadMimi.new("sarahsponda902@gmail.com", 'df65cf0a215c2b3028fa7eaf89a6f2ba').send_mail(@options, @yaml_body)
+    end
     respond_to do |format|
       if @mad_mimi_email.save
         format.html { redirect_to @mad_mimi_email, notice: 'Mad mimi email was successfully created.', :notice => @saved }
