74a6ffcc637ffa0701a532ed689bb495cb0baf52
Sarah Adams <sarahsponda902@gmail.com>
2012-07-16 08:19:34 -0700

    G

--- app/models/organization_account.rb
+++ app/models/organization_account.rb
@@ -1,12 +1,28 @@
 class OrganizationAccount < ActiveRecord::Base
   validates_exclusion_of :organization_id, :in => OrganizationAccount.all.map(&:organization_id)
-
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 0
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :position, :type_of_company, :nonprofit, :username, :notify, :country, :organization_id, :organization_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :position, :type_of_company, :nonprofit, :username, :notify, :country, :organization_id, :organization_name, :admin_pass
   
+  def validates_admin_pass
+    @admin_pass = Digest::SHA1.hexdigest("#{salt}:#{:admin_pass}") 
+    if @admin_pass != encrypted_pass
+      errors.add_to_base("You must be an administrator to send an organization account invitation.")
+    end
+  end
+  
+  private
+  
+    def salt
+      return "meeQue8Zucijoo7"
+    end
+    
+    def encrypted_pass
+      return "4e5d0ed9183ebf2fed541412497e15a30e72f9cb"
+    end
 end
