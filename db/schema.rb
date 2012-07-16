0cd0b51aa61e48857b846897e05d49f66f00bb83
Sarah Adams <sarahsponda902@gmail.com>
2012-07-16 00:07:26 -0700

    G

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716053556) do
+ActiveRecord::Schema.define(:version => 20120716070657) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -411,10 +411,6 @@ ActiveRecord::Schema.define(:version => 20120716053556) do
     t.boolean  "crops"
     t.integer  "unread_messages"
     t.string   "return_link"
-    t.boolean  "org"
-    t.string   "org_pass"
-    t.boolean  "org_update"
-    t.integer  "organization_id"
   end
 
   add_index "users", ["approved"], :name => "index_users_on_approved"
