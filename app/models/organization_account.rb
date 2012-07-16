0c687370c9484a0d5b1b3061d3be78637b6bb7dd
Sarah Adams <sarahsponda902@gmail.com>
2012-07-16 08:05:29 -0700

    G

--- app/models/organization_account.rb
+++ app/models/organization_account.rb
@@ -1,7 +1,5 @@
 class OrganizationAccount < ActiveRecord::Base
   validates_exclusion_of :organization_id, :in => OrganizationAccount.all.map(&:organization_id)
-  validates_presence_of :invitation_token
-  
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
@@ -10,5 +8,5 @@ class OrganizationAccount < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :position, :type_of_company, :nonprofit, :username, :notify, :country, :organization_id, :organization_name
-
+  
 end
