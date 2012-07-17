c876b935fad9a334b3968861881e741262c49378
Sarah Adams <sarahsponda902@gmail.com>
2012-07-16 23:20:10 -0700

    G

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,10 +5,10 @@ class Contact < ActiveRecord::Base
   
   def present_email_and_body
     if self.is_request.nil?
-      if body.nil?
+      if self.body.nil?
         errors.add(:body, "cannot be blank")
       end
-      if email.nil?
+      if self.email.nil?
         errors.add(:email, "cannot be blank")
       end
     end
@@ -16,7 +16,7 @@ class Contact < ActiveRecord::Base
   
   def length_of_body
     if self.is_request.nil?
-      if body.nil? || body.length < 5
+      if self.body.nil? || self.body.length < 5
         errors.add(:body, "must be more than 5 characters")
       end
     end
@@ -25,7 +25,7 @@ class Contact < ActiveRecord::Base
   def format_of_email
     @email_format = Regexp.new(/^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/)
     if self.is_request.nil?
-      if !(@email_format.match(email))
+      if self.email.nil? || !(@email_format.match(self.email))
         errors.add(:email, "must be a valid email address")
       end
     end
