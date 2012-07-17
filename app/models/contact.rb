4263e92c00948aeaed2fe5c344cc423ca19f1dfd
Sarah Adams <sarahsponda902@gmail.com>
2012-07-16 22:19:07 -0700

    G

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,4 @@
 class Contact < ActiveRecord::Base
-  validates_presence_of :email, :body, :unless => :to_whom == "request"
-  validates_length_of :body, :minimum => 5, :unless => :to_whom == "request"
-  validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i, :unless => :to_whom == "request"
-end
+  validates_presence_of :email, :body, :unless => Proc.new {|contact| contact.to_whom == "request"}
+  validates_length_of :body, :minimum => 5, :unless => Proc.new {|contact| contact.to_whom == "request"}
+  validates_format_of :email, :with => /^([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})$/i, :unless => Proc.new {|contact| contact.to_whom == "request"}
