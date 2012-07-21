419e993478820f867d39a3ce9a02d6fe74567c8d
Sarah Adams <sarahsponda902@gmail.com>
2012-07-21 12:10:29 -0700

    G

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -9,7 +9,7 @@ has_many :programs, :dependent => :destroy
 has_one :organization_account, :dependent => :destroy
 attr_accessible :image, :name, :description, :show, :overall, :num_reviews, :program_id, :page_views, :misson, :phone, :email, :operating_since, :num_vols_date, :num_vols_yr, :application_process, :business_model, :program_model_string, :good_to_know, :reviews_count, :trining_resources, :price_ranges, :program_costs_breakdown, :program_costs_includes, :url, :run_by, :id_number, :volunteer_program_model, :price_ranges, :training_resources, :crop_x, :crop_y, :crop_w, :crop_h, :square_image, :program_costs_doesnt_include, :price_breakdown, :headquarters_location, :types_of_programs, :will_invite, :invite_email, :published_docs
 validates_presence_of :image
-validate :published_docs_true
+
 
 scope :random, :order=>'RAND()', :limit=>1
 validates_uniqueness_of :name
@@ -47,10 +47,6 @@ def square_image_crop
    end
  end
  
- def published_docs_true
-   if !(published_docs) && !(price_ranges.nil?) && !(price_breakdown.nil?)
-     errors.add(:published_docs, "must be published")
-   end
- end
+ 
 
 end
