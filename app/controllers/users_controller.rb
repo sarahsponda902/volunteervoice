c9c814faf07f648887ea722dc3349acd1e50b577
Sarah Adams <sarahsponda902@gmail.com>
2012-07-23 19:00:18 -0700

    G

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   
   include ActionView::Helpers::TextHelper
 	require 'aws/s3'
-
+  
+  helper :all
+  helper_method :age
   
   def crop
     @user = User.find(params[:id])
