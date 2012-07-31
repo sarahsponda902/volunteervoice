e7b8fa224b0afa267f9cc50a5ec771879a3c66fa
Sarah Adams <sarahsponda902@gmail.com>
2012-07-30 18:28:16 -0700

    G

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -439,8 +439,7 @@ end
   def update
     @organization = Organization.find(params[:id])
       if (user_signed_in? && current_user.admin?) || (!current_organization_account.nil? && @organization.id == current_organization_account.organization_id)
-        params[:organization][:description] = RedCloth.new( ActionController::Base.helpers.sanitize( params[:organization][:description] ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
-         respond_to do |format|
+        respond_to do |format|
            if @organization.update_attributes(params[:organization])
              format.html { redirect_to "/organizations/#{@organization.id}" }
              format.json { respond_with_bip(@organization) }
