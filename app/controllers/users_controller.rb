e7273fc438eadda2e4460ba5b4053deff3dceef4
Sarah Adams <sarahsponda902@gmail.com>
2012-08-09 14:24:11 -0700

    G

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,7 +72,7 @@ class UsersController < ApplicationController
           flash[:notice] = flash[:notice].to_a.concat @user.errors.full_messages
           flash.now[:notice]
           respond_to do |format|
-            format.html { render :action => "edit" }
+            format.html { render :action => "show", :id => @user.id }
             format.json { render json: @user.errors, status: :unprocessable_entity }
           end
         end
