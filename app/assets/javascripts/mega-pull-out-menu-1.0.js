f61bd1767e0ca7f740fb420b51e91979281a4247
Sarah Adams <sarahsponda902@gmail.com>
2012-08-09 22:06:27 -0700

    G

--- app/assets/javascripts/mega-pull-out-menu-1.0.js
+++ app/assets/javascripts/mega-pull-out-menu-1.0.js
@@ -82,7 +82,7 @@
     }
 
     // button
-    var tmp_button = ['subject', 'region', 'length', 'size', 'cost'];
+    var tmp_button = ['folder', 'clock', 'man', 'piggy', 'world'];
     if ($.inArray(options.button, tmp_button) !== -1) {
 
         options.button = '/assets/' + options.button + '.png';
