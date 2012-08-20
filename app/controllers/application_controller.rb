3eae7d3f4fd9d22e3713e08a78e4a2892c4e16eb
Sarah Adams <sarahsponda902@gmail.com>
2012-08-19 22:27:31 -0700

    G

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   include SimpleCaptcha::ControllerHelpers
 
-#devise path for "after signing in"
+  #devise path for "after signing in"
   def after_sign_in_path_for(resource)
     if request.referrer
       case URI(request.referrer).path
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
 
   end
 
-#devise path for "after signing out"
+  #devise path for "after signing out"
   def after_sign_out_path_for(resource)
     if request.referrer
       case URI(request.referrer).path 
@@ -33,17 +33,28 @@ class ApplicationController < ActionController::Base
   end
 
 
-#render pretty error pages when 404 or 500 status code appear
-  def render_optional_error_file(status_code)
-    case status_code
-    when :not_found then
-      render :controller => "errors", :action => "error_404"
-    when "500" then
-     render :controller => "errors", :action => "error_500"
-    else
-      super
-    end
+  #render pretty error pages when 404 or 500 status code appear
+  unless ActionController::Base.consider_all_requests_local
+    rescue_from Exception, :with => :render_error
+    rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+    rescue_from ActionController::RoutingError, :with => :render_not_found
+    rescue_from ActionController::UnknownController, :with => :render_not_found
+    rescue_from ActionController::UnknownAction, :with => :render_not_found
+  end
 
+
+  private
+
+  def render_not_found(exception)
+    log_error(exception)
+    notify_airbrake(exception)
+    render :controller => "errors", :action => "error_404", :status => 404
+  end
+
+  def render_error(exception)
+    log_error(exception)
+    notify_airbrake(exception)
+    render :controller => "errors", :action => "error_500", :status => 500
   end
 
 end
