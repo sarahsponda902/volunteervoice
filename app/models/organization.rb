77141a0e19991aab3f646e505d16deda9bae48d6
Sarah Adams <sarahsponda902@gmail.com>
2012-08-21 00:53:37 -0700

    G

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -63,7 +63,11 @@ end
 
 def square_image_crop
    if !(self.crop_x.nil? || self.crop_y.nil? || self.crop_w.nil? || self.crop_h.nil?)
-     image = MiniMagick::Image.open(self.image.url)
+     image = MiniMagick::Image.open(self.photo.url)
+      if image[:width] > 700
+        resize_scale = 700/image[:width]
+        image = image.sample(resize_scale.to_s + "%")
+      end
      image.crop("#{self.crop_w}x#{self.crop_h}+#{self.crop_x}+#{self.crop_y}")
      image.set("page", "#{self.crop_w}x#{self.crop_h}+#{self.crop_x}+#{self.crop_y}") 
      self.square_image = image
