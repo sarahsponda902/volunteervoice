7023f8904268fcab2ae0735ca029bee65c27bf67
Sarah Adams <sarahsponda902@gmail.com>
2012-08-21 11:24:11 -0700

    G

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,7 +49,6 @@ class ApplicationController < ActionController::Base
     rescue_from ActionController::RoutingError, :with => :render_not_found
     rescue_from ActionController::UnknownController, :with => :render_not_found
     rescue_from AbstractController::ActionNotFound, :with => :render_not_found
-    rescue_from ActionController::UnknownAction, :with => :render_not_found
   end
 
 
