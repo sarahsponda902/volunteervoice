465381e154f191384eef6dbadc1a5a0e20864d0e
Sarah Adams <sarahsponda902@gmail.com>
2012-08-20 23:17:37 -0700

    G

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ActiveRecord::Base
 require 'file_size_validator'
+  include CarrierWave::MiniMagick
 @orgs = []
-Organization.all.each do |f|
+Organization.find_each do |f|
   @orgs << f.name
 end
 validates :terms, :acceptance => {:accept => true}
@@ -22,7 +23,7 @@ validates :photo10, :file_size => {:maximum => 0.5.megabytes.to_i}
 belongs_to :program
 belongs_to :user
 has_many :flags, :dependent => :destroy
-#before_save :resize_review_photos
+before_create :resize_review_photos
 
 
 
@@ -41,6 +42,130 @@ mount_uploader :photo8, ImageUploader
 mount_uploader :photo9, ImageUploader
 mount_uploader :photo10, ImageUploader
 
+def resize_review_photos
+  if !photo.blank?
+    image = MiniMagick::Image.open(self.photo.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo = image
+  end
+  if !photo2.blank?
+    image = MiniMagick::Image.open(self.photo2.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo2 = image
+  end
+  if !photo3.blank?
+    image = MiniMagick::Image.open(self.photo3.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo3 = image
+  end
+  if !photo4.blank?
+    image = MiniMagick::Image.open(self.photo4.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo4 = image
+  end
+  if !photo5.blank?
+    image = MiniMagick::Image.open(self.photo5.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo5 = image
+  end
+  if !photo6.blank?
+    image = MiniMagick::Image.open(self.photo6.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo6 = image
+  end
+  if !photo7.blank?
+    image = MiniMagick::Image.open(self.photo7.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo7 = image
+  end
+  if !photo8.blank?
+    image = MiniMagick::Image.open(self.photo8.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo8 = image
+  end
+  
+  if !photo9.blank?
+    image = MiniMagick::Image.open(self.photo9.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo9 = image
+  end
+  if !photo10.blank?
+    image = MiniMagick::Image.open(self.photo10.url)
+    if image[:width] > 600
+      scaling_width_percent = (image[:width] / 600)*100.round - 5
+      image = image.sample(scaling_width_percent.to_s + "%")
+    end  
+    if image[:height] > 300
+      scaling_height_percent = (image[:height] / 300)*100.round - 5
+      image = image.sample(scaling_height_percent.to_s + "%")
+    end
+    self.photo10 = image
+  end
+  
+end
 
 
 end
