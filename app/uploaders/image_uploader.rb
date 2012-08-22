581e312ad6bfc973cc352eff3086dea9f626b6e8
Sarah Adams <sarahsponda902@gmail.com>
2012-08-21 21:10:03 -0700

    creating thumb versions for review photos

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,9 +35,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   def resize_to_fit(width, height)
     manipulate! do |img|
       if img[:width] > 700
-      img.resize "#{width}x#{height}"
-      img = yield(img) if block_given?
-      img
+        img.resize "#{width}x#{height}"
+        img = yield(img) if block_given?
+        img
+      end
     end
   end
 
