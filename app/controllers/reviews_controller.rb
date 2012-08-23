5c0d05eb4aa5e0ff7dfbe46f2cfc3c7bd62f4abc
Sarah Adams <sarahsponda902@gmail.com>
2012-08-22 22:21:26 -0700

    G

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -48,7 +48,7 @@ include ActionView::Helpers::TextHelper
        end
        else
          @review_id = Review.where(:program_id => @review.program_id, :user_id => current_user.id).first.id
-         redirect_to "/reviews/#{@review_id}/edit", :edit_review_body => @review.body.gsub(%r{</?[^>]+?>}, '')
+         redirect_to(edit_review_path(:id => @review_id, :notice => ["You can only review a program once. Please add on to your review instead."], :edit_review_body => @review.body.gsub(%r{</?[^>]+?>}, '')))
        end
      else
        redirect_to "/registrations/mustBe"
