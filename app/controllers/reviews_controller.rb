e8f1d2d11a0cf9febf4e1190ee6abf68c6b8322d
Sarah Adams <sarahsponda902@gmail.com>
2012-08-23 13:53:41 -0700

    added error messages to review prog once

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
-include ActionView::Helpers::TextHelper
-  
+  include ActionView::Helpers::TextHelper
+
   def show_all
     @sort_by = params[:sort_by]
     if @sort_by.nil? || @sort_by == "rating_high"
@@ -12,54 +12,55 @@ include ActionView::Helpers::TextHelper
     end
     @flag = Flag.new
   end
-  
-   def create
-     if user_signed_in?
+
+  def create
+    if user_signed_in?
       @review = Review.new(params[:review])
-  
+
       @review.truncated100 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate @review.body, :length => 100) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
-       @review.truncated200 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate @review.body, :length => 200) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
+      @review.truncated200 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate @review.body, :length => 200) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
       @review.body = RedCloth.new( ActionController::Base.helpers.sanitize( @review.body ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
       @review.organization_id = Organization.where(:name => @review.organization_name).first.id
-      
+
       @user_review_progs = []
-        current_user.reviews.each do |f|
-          @user_review_progs << f.program_id
-        end
-    if !(@user_review_progs.include?(@review.program_id))
-       respond_to do |format|
-         if @review.save
-           @prog = Program.find(@review.program_id)
+      current_user.reviews.each do |f|
+        @user_review_progs << f.program_id
+      end
+      if !(@user_review_progs.include?(@review.program_id))
+        respond_to do |format|
+          if @review.save
+            @prog = Program.find(@review.program_id)
             @prog.overall = (((@prog.overall.to_f)*(@prog.reviews.count.to_f) + @review.overall.to_f)/(@prog.reviews.count.to_f + 1))
             @prog.save
             @org = Organization.find(@review.organization_id)
-             @org.overall = (((@org.overall.to_f)*(@org.reviews_count.to_f) + @review.overall.to_f)/(@org.reviews_count.to_f + 1))
-             @org.reviews_count = @org.reviews_count + 1
-             @org.save
-             format.html { redirect_to "/reviews/thank_you_review" }
-             format.json { render json: @review, status: :created, location: @review }
-         else
-           @review.body = @review.body.gsub(%r{</?[^>]+?>}, '') unless @review.body.nil?
-           flash[:notice] = flash[:notice].to_a.concat @review.errors.full_messages
+            @org.overall = (((@org.overall.to_f)*(@org.reviews_count.to_f) + @review.overall.to_f)/(@org.reviews_count.to_f + 1))
+            @org.reviews_count = @org.reviews_count + 1
+            @org.save
+            format.html { redirect_to "/reviews/thank_you_review" }
+            format.json { render json: @review, status: :created, location: @review }
+          else
+            @review.body = @review.body.gsub(%r{</?[^>]+?>}, '') unless @review.body.nil?
+            flash[:notice] = flash[:notice].to_a.concat @review.errors.full_messages
 
-             format.html { render action: "new" }
-             format.json { render json: @review.errors, status: :unprocessable_entity }
-         end
-       end
-       else
-         @review_id = Review.where(:program_id => @review.program_id, :user_id => current_user.id).first.id
-         redirect_to(edit_review_path(:id => @review_id, :notice => "You can only review a program once. Please add on to your review instead.", :edit_review_body => @review.body.gsub(%r{</?[^>]+?>}, '')))
-       end
-     else
-       redirect_to "/registrations/mustBe"
-     end
+            format.html { render action: "new" }
+            format.json { render json: @review.errors, status: :unprocessable_entity }
+          end
+        end
+      else
+        @review_id = Review.where(:program_id => @review.program_id, :user_id => current_user.id).first.id
+        flash[:notice] = flash[:notice].to_a.concat(@review.errors.full_messages).concat(["You can only review a program once. Please add on to your review instead!"])
+        redirect_to(edit_review_path(:id => @review_id, :edit_review_body => @review.body.gsub(%r{</?[^>]+?>}, '')))
+      end
+    else
+      redirect_to "/registrations/mustBe"
     end
-    
-    def thank_you_review
-    end
-    
-    def index
-      if (user_signed_in? && current_user.admin?)
+  end
+
+  def thank_you_review
+  end
+
+  def index
+    if (user_signed_in? && current_user.admin?)
       @reviews = Review.all.reverse
 
       respond_to do |format|
@@ -69,151 +70,151 @@ include ActionView::Helpers::TextHelper
     else
       redirect_to root_path
     end
+  end
+
+
+  def show
+    @review = Review.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render :json => @review}
     end
-    
+  end
 
-     def show
-       @review = Review.find(params[:id])
+  def new
+    @review = Review.new
+    if user_signed_in?
+      @review.organization_id = params[:organization_id]
+      @review.program_id = params[:program_id]
 
-       respond_to do |format|
-         format.html # show.html.erb
-         format.json { render :json => @review}
-       end
-     end
 
-     def new
-       @review = Review.new
-       if user_signed_in?
-       @review.organization_id = params[:organization_id]
-       @review.program_id = params[:program_id]
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render :json => @review }
+      end
+    else
+      redirect_to "/registrations/mustBe"
+    end
+  end
 
-       
-       respond_to do |format|
-         format.html # new.html.erb
-         format.json { render :json => @review }
-       end
-     else
-       redirect_to "/registrations/mustBe"
-     end
-     end
+  def edit
+    @review = Review.find(params[:id])
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @review }
+    end
+  end
 
-     def edit
-       @review = Review.find(params[:id])
-       respond_to do |format|
-         format.html # new.html.erb
-         format.json { render :json => @review }
-       end
-     end
+  def update
+    @review = Review.find(params[:id])
+    @review.truncated100 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate params[:review][:body], :length => 100) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
+    @review.truncated200 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate params[:review][:body], :length => 200) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
 
-     def update
-       @review = Review.find(params[:id])
-        @review.truncated100 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate params[:review][:body], :length => 100) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
-        @review.truncated200 = RedCloth.new( ActionController::Base.helpers.sanitize( (truncate params[:review][:body], :length => 200) ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html
+    params[:review][:body] = "<strong>Update #{Time.now.to_date.strftime("%m/%e/%Y").gsub(/^0/, '')}:</strong> "+RedCloth.new( ActionController::Base.helpers.sanitize( params[:review][:body] ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html + "<br />"+@review.body
 
-        params[:review][:body] = "<strong>Update #{Time.now.to_date.strftime("%m/%e/%Y").gsub(/^0/, '')}:</strong> "+RedCloth.new( ActionController::Base.helpers.sanitize( params[:review][:body] ), [:filter_html, :filter_styles, :filter_classes, :filter_ids] ).to_html + "<br />"+@review.body
-       
-       if user_signed_in? && (current_user.admin? || current_user.id == @review.user_id)
+    if user_signed_in? && (current_user.admin? || current_user.id == @review.user_id)
 
-         respond_to do |format|
-           if @review.update_attributes(params[:review])
-             @review.save
-             format.html { redirect_to "/users/profile", :notice => 'Review Submitted' }
-             format.json { head :no_content }
-           else
-             params[:review][:body] = @review.body
-             flash[:notice] = flash[:notice].to_a.concat @review.errors.full_messages
-             format.html { render :action => "edit" }
-             format.json { render :json => @review.errors, :status => :unprocessable_entity }
-           end
-         end
-       else
-         redirect_to root_path
-       end
-     end
-
-     def destroy
-        @review = Review.find(params[:id])
-        if (user_signed_in? && (current_user.admin? || current_user.id == @review.user_id))
-         @review_id = @review.program_id
-         @field = params[:field]
-         @location = params[:location]
-         @org = Organization.find(@review.organization_id)
-         @prog = Program.find(@review.program_id)
-         
-         if @org.reviews_count != 1
-           @org.overall = (@org.overall * @org.reviews_count - @review.overall) / (@org.reviews_count - 1)
-         else
-           @org.overall = 0
-         end
-           @org.reviews_count = @org.reviews_count - 1
-         if @prog.reviews.count != 1
-           @prog.overall = (@prog.overall * @prog.reviews.count - @review.overall) / (@prog.reviews.count - 1)
-         else 
-           @prog.overall = 0
-         end
-         @org.save
-         @prog.save
-         
-         @review.destroy
-
-         respond_to do |format|
-           if current_user.admin?
-             format.html { redirect_to reviews_path }
-           else
-             format.html { redirect_to "/users/profile" }
-           end
-           format.json { head :no_content }
-         end
-       else
-         redirect_to root_path
-       end
-
-     end
-     
-     def make
-       @review = Review.find(params[:id])
-        respond_to do |format|
-          format.html # new.html.erb
-          format.json { render :json => @review }
+      respond_to do |format|
+        if @review.update_attributes(params[:review])
+          @review.save
+          format.html { redirect_to "/users/profile", :notice => 'Review Submitted' }
+          format.json { head :no_content }
+        else
+          params[:review][:body] = @review.body
+          flash[:notice] = flash[:notice].to_a.concat @review.errors.full_messages
+          format.html { render :action => "edit" }
+          format.json { render :json => @review.errors, :status => :unprocessable_entity }
         end
       end
-      
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    if (user_signed_in? && (current_user.admin? || current_user.id == @review.user_id))
+      @review_id = @review.program_id
+      @field = params[:field]
+      @location = params[:location]
+      @org = Organization.find(@review.organization_id)
+      @prog = Program.find(@review.program_id)
+
+      if @org.reviews_count != 1
+        @org.overall = (@org.overall * @org.reviews_count - @review.overall) / (@org.reviews_count - 1)
+      else
+        @org.overall = 0
+      end
+      @org.reviews_count = @org.reviews_count - 1
+      if @prog.reviews.count != 1
+        @prog.overall = (@prog.overall * @prog.reviews.count - @review.overall) / (@prog.reviews.count - 1)
+      else 
+        @prog.overall = 0
+      end
+      @org.save
+      @prog.save
+
+      @review.destroy
+
+      respond_to do |format|
+        if current_user.admin?
+          format.html { redirect_to reviews_path }
+        else
+          format.html { redirect_to "/users/profile" }
+        end
+        format.json { head :no_content }
+      end
+    else
+      redirect_to root_path
+    end
+
+  end
+
+  def make
+    @review = Review.find(params[:id])
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render :json => @review }
+    end
+  end
+
   def changeShow
-      if (user_signed_in? && current_user.admin?)
-        @review = Review.find(params[:id])
-        if @review.show
-          @review.show = false
-          @review.save
-        else
-          @review.show = true
-          @review.save
-        end
-          respond_to do |format|
-            format.html {redirect_to reviews_path}
-          end
+    if (user_signed_in? && current_user.admin?)
+      @review = Review.find(params[:id])
+      if @review.show
+        @review.show = false
+        @review.save
       else
-        redirect_to root_path
+        @review.show = true
+        @review.save
       end
+      respond_to do |format|
+        format.html {redirect_to reviews_path}
+      end
+    else
+      redirect_to root_path
+    end
   end
-  
+
   def changeFlagShow
-      if (user_signed_in? && current_user.admin?)
-        @review = Review.find(params[:id])
-        if @review.flag_show
-          @review.flag_show = false
-          @review.save
-        else
-          @review.flag_show = true
-          @review.save
-        end
-          respond_to do |format|
-            format.html {redirect_to flags_path}
-          end
+    if (user_signed_in? && current_user.admin?)
+      @review = Review.find(params[:id])
+      if @review.flag_show
+        @review.flag_show = false
+        @review.save
       else
-        redirect_to root_path
+        @review.flag_show = true
+        @review.save
       end
+      respond_to do |format|
+        format.html {redirect_to flags_path}
+      end
+    else
+      redirect_to root_path
+    end
   end
-  
+
   def already_reviewed
   end
 
