fe0f3fdd6afbd26495c020984e689f319f2db8e6
Sarah Adams <sarahsponda902@gmail.com>
2012-11-02 20:05:48 -0700

    organization accounts edit by admin fix

--- app/models/organization_account.rb
+++ app/models/organization_account.rb
@@ -5,7 +5,8 @@ class OrganizationAccount < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :lockable,
-         :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 0, :authentication_keys => [:login]
+         :recoverable, :rememberable, :trackable, :validatable, :invitable, :invite_for => 0, :authentication_keys => [:login],
+         :resend_invitation
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :email_confirmation, :password, :password_confirmation, :remember_me, :first_name, :last_name, :position, :type_of_company, :nonprofit, :username, :notify, :country, :organization_id, :organization_name, :admin_pass, :login
