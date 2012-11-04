8efc241d00136c8618cae1b22f5db196aee6c621
Sarah Adams <sarahsponda902@gmail.com>
2012-11-03 17:48:12 -0700

    trying to fix login error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :confirmable, :lockable
+         :recoverable, :rememberable, :trackable, :confirmable, :lockable, :validatable
 
   # Setup accessible (or protected) attributes for your model
  
