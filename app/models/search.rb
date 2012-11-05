a8eacda97a6433f64c71ceaca809bd6ff5f8540a
Sarah Adams <sarahsponda902@gmail.com>
2012-11-05 13:49:24 -0800

    trying to fix program browse search

--- app/models/search.rb
+++ app/models/search.rb
@@ -24,10 +24,9 @@ class Search < ActiveRecord::Base
   # does all the searching and returns results based on the parameters above
   # searches through 3 models: programs, organizations, and cost-length-maps
   def search_results(id)
-    Rails.logger.info "\ngot to search results\n"
     @search = Search.find(id)
     
-    Rails.logger.info "\nadding params"
+
     # set the search parameters for sunspot to use
     keys = @search.keywords
     subjects = @search.subjects.nil? ? [] : @search.subjects.split("; ")
@@ -36,19 +35,19 @@ class Search < ActiveRecord::Base
     price_max = @search.price_max.nil? ? 0 : @search.price_max
     price_min = @search.price_min.nil? ? 99999 : @search.price_min
     
-    Rails.logger.info "\nadding length min"
+
       # set the minimum length
     @search.length_min_number ||= 0
     @search.length_min_param ||= "weeks"
     length_min = @search.length_min_number.to_i.send(@search.length_min_param).to_f
     
-    Rails.logger.info "\nadding length max"
+
       # set the maximum length
     @search.length_max_number ||= 2
     @search.length_max_param ||= "years"
     length_max = @search.length_max_number.to_i.send(@search.length_max_param).to_f
 
-    Rails.logger.info "\ndoing search 1"
+
     # search for programs with the given params
     @program_search = Program.search do
       keywords keys unless keys.blank?
@@ -65,7 +64,6 @@ class Search < ActiveRecord::Base
 
     # if the user has changed the default prices, search through cost-length maps
     if (price_max != 99999 || price_min != 0 || length_min != 0.weeks.to_f || length_max != 2.years.to_f )
-      Rails.logger.info "\n doing search 2"
       # Cost Length search
       @cost_length_search = ProgramCostLengthMap.search do
         all_of do
@@ -76,11 +74,11 @@ class Search < ActiveRecord::Base
         end      
       end
 
-      Rails.logger.info "\niterating through results"
+
       # iterate through cost length maps and get the programs associated with them
       @program_cost_length_results = @cost_length_search.results.map {|res| Program.find(res.program_id)}.uniq
 
-      Rails.logger.info "\n intersection of results"
+
       # union (without duplicates) of program_cost_length_results and program_results
       @final_results = @program_cost_length_results | @program_results
     else # if no search through cost_length_maps
@@ -95,7 +93,6 @@ class Search < ActiveRecord::Base
       @final_results = results.map{|res| Organization.find(p.organization_id)}.uniq
     end
 
-    Rails.logger.info "\n searching number 3"
     # do a third search, to determine if there are organizations we missed because they have no programs
     @second_search = Organization.search do 
       keywords keys unless keys.nil?
@@ -107,13 +104,12 @@ class Search < ActiveRecord::Base
     # add second_search results to final_results
     @final_results = @final_results | @second_search.results
 
-    Rails.logger.info "\n sorting results"
     #sort results given @search.sort_by   
-    @final_results.sort_by!(&:overall).reverse! if @search.sort_by == "ratinghigh"
-    @final_results.sort_by!(&:overall) if @search.sort_by == "ratinglow"
-    @final_results.sort_by!(&:name) if @search.sort_by == "alphabetical"
-    @final_results.sort_by!(&:weekly_cost) if @search.sort_by == "pricelow"
-    @final_results.sort_by!(&:weekly_cost).reverse! if @search.sort_by == "pricehigh"
+    @final_results = @final_results.sort_by(&:overall).reverse! if @search.sort_by == "ratinghigh"
+    @final_results = @final_results.sort_by(&:overall) if @search.sort_by == "ratinglow"
+    @final_results = @final_results.sort_by(&:name) if @search.sort_by == "alphabetical"
+    @final_results = @final_results.sort_by(&:weekly_cost) if @search.sort_by == "pricelow"
+    @final_results = @final_results.sort_by(&:weekly_cost).reverse! if @search.sort_by == "pricehigh"
 
     # return results... WE'RE DONE!!!
     @final_results
