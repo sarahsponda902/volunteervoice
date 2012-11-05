ccaa2c0fc7e47b758e8bd1ac2990bd07234ce24d
Sarah Adams <sarahsponda902@gmail.com>
2012-11-05 12:15:15 -0800

    fixing org show

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -188,10 +188,8 @@ class OrganizationsController < ApplicationController
 
   ## check_for_admin_or_org_account called by before_filter
   def check_for_admin_or_org_account
-    if current_organization_account.nil? 
-      if (current_user.present? && !(current_user.admin?)) || current_user.nil?
-        redirect_to root_path
-      end
+    if !((user_signed_in? && current_user.admin?) || (current_organization_account.present?))
+      redirect_to root_path
     end
   end
 end
