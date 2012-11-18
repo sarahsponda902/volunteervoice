15b98c2655eed72d63af04a4bac79780bf057290
Sarah Adams <adams.sarah@berkeley.edu>
2012-11-17 21:42:38 -0800

    fixing rails root in blog kit

--- app/helpers/blog_kit_model_helper.rb
+++ app/helpers/blog_kit_model_helper.rb
@@ -3,6 +3,7 @@
 ### in blog_kit plugin at:  https://github.com/ryanstout/blog_kit #####
 #######################################################################
 
+
 module BlogKitModelHelper
 	
 end
