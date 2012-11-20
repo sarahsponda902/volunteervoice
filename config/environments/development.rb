cc3a531821ba190a8344989c564ec5b570e3017f
Sarah Adams <adams.sarah@berkeley.edu>
2012-11-20 15:14:56 -0800

    fixing org show

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ RMTest::Application.configure do
   
   # Actionmailer settings
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :sendmail #:smtp
+  config.action_mailer.delivery_method = :sendmail 
   config.action_mailer.default_url_options = { :host => 'volunteervoice.org' }
   
 end
